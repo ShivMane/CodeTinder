bc28fd428e0381b4be27cff015a09f67f233bd36
ShivMane <shivprasadmane190@gmail.com>
2025-11-12 13:25:19 +0530

    Exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -6,10 +6,42 @@ app.listen(7777, () => {
     console.log("Server is Successfully listining on port 7777...")
 })
 
-app.use("/test",(req,res) => {
-    res.send("Hello From Server...")
+app.use("/user", (req,res) => {
+    res.send("HAHAAHAHAHAHAHAHAHAHAH")
 })
 
-app.use("/test1",(req,res) => {
-    res.send("Hello From Server... test1")
+// THis will Only handle the /GET call to the user
+app.get("/user", (req,res) => {
+    res.send({ firstname: "Shivprasad", lastname: "Mane"});
+});
+
+// THis will Only handle the /POST call to the user
+app.post("/user", (req,res) => {
+    // Saving data to db
+    res.send("Data successfully stored to database")
 })
+
+app.delete("/user", (req,res) => {
+    // Deleting Data
+    res.send("Data Deleted Succcessfully...")
+})
+
+app.use("/hello",(req,res) => {
+    res.send("Hello hello hello")
+})
+
+// app.use("/test",(req,res) => {
+//     res.send("Hello From Server...")
+// })
+
+// app.use("/",(req,res) => {
+//     res.send("Hello Shiv...")
+// })
+
+// app.use("/test1",(req,res) => {
+//     res.send("Hello From Server... test1")
+// })
+
+// app.use("/",(req,res) => {
+//     res.send("Hello Shiv...")
+// })
