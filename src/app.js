2832001d4e54fb9d853666bfc45d0a835ae12ead
ShivMane <shivprasadmane190@gmail.com>
2025-11-13 17:29:14 +0530

    Explorind auth middleware

--- src/app.js
+++ src/app.js
@@ -2,23 +2,39 @@ const express = require("express");
 
 const app = express();
 
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req,res) => {
+    res.send("User data send");
+})
+
+app.get("/admin/getalldata", (req,res) => {
+    res.send("All Data Sent");
+})
+
+app.get("/admin/deleteuser", (req,res) => {
+    res.send("Deleted a User");
+})
+
 app.listen(7777, () => {
     console.log("Server is Successfully listining on port 7777...")
 })
 
 
-app.use("/user", 
-    (req,res, next) => {
-        // next();
-        console.log("Handling the route user 1");
-        res.send("1st Response...");
-        // next();
-    },
-    (req,res) => {
-        console.log("Handling the route user 2");
-        res.send("2nd Response..."); 
-    }
-);
+// app.use("/user", 
+//     (req,res, next) => {
+//         // next();
+//         console.log("Handling the route user 1");
+//         res.send("1st Response...");
+//         // next();
+//     },
+//     (req,res) => {
+//         console.log("Handling the route user 2");
+//         res.send("2nd Response..."); 
+//     }
+// );
 
 
 // app.use("/user", (req,res) => {
