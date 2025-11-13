9308c030d4e409d77d4e4f7774623395c757f0dd
ShivMane <shivprasadmane190@gmail.com>
2025-11-13 12:45:43 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -6,29 +6,44 @@ app.listen(7777, () => {
     console.log("Server is Successfully listining on port 7777...")
 })
 
-app.use("/user", (req,res) => {
-    res.send("HAHAAHAHAHAHAHAHAHAHAH")
-})
 
-// THis will Only handle the /GET call to the user
-app.get("/user", (req,res) => {
-    res.send({ firstname: "Shivprasad", lastname: "Mane"});
-});
+app.use("/user", 
+    (req,res, next) => {
+        // next();
+        console.log("Handling the route user 1");
+        res.send("1st Response...");
+        // next();
+    },
+    (req,res) => {
+        console.log("Handling the route user 2");
+        res.send("2nd Response..."); 
+    }
+);
 
-// THis will Only handle the /POST call to the user
-app.post("/user", (req,res) => {
-    // Saving data to db
-    res.send("Data successfully stored to database")
-})
 
-app.delete("/user", (req,res) => {
-    // Deleting Data
-    res.send("Data Deleted Succcessfully...")
-})
+// app.use("/user", (req,res) => {
+//     res.send("HAHAAHAHAHAHAHAHAHAHAH")
+// })
 
-app.use("/hello",(req,res) => {
-    res.send("Hello hello hello")
-})
+// // THis will Only handle the /GET call to the user
+// app.get("/user", (req,res) => {
+//     res.send({ firstname: "Shivprasad", lastname: "Mane"});
+// });
+
+// // THis will Only handle the /POST call to the user
+// app.post("/user", (req,res) => {
+//     // Saving data to db
+//     res.send("Data successfully stored to database")
+// })
+
+// app.delete("/user", (req,res) => {
+//     // Deleting Data
+//     res.send("Data Deleted Succcessfully...")
+// })
+
+// app.use("/hello",(req,res) => {
+//     res.send("Hello hello hello")
+// })
 
 // app.use("/test",(req,res) => {
 //     res.send("Hello From Server...")
